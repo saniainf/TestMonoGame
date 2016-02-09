93fd5568acbb010779a5cae3e65a774c018f7375
sania <saniainf@gmail.com>
2016-02-09 18:07:06 +0600

    egg new

---
+++ EggGame/EggGame/Sprite.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EggGame
+{
+    public class Sprite
+    {
+        public Vector2 Location
+        {
+            get { return spritePosition; }
+            set { spritePosition = value; }
+        }
+
+        public int Width
+        {
+            get { return spriteRectangle.Width; }
+            set { spriteRectangle.Width = value; }
+        }
+
+        public int Height
+        {
+            get { return spriteRectangle.Height; }
+            set { spriteRectangle.Height = value; }
+        }
+
+        private List<Rectangle> frames = new List<Rectangle>();
+        private int currentFrame;
+        private Rectangle spriteRectangle;
+        private Vector2 spritePosition;
+        private Texture2D spriteTexture;
+        
+
+        public Sprite(Point size, Point location, Texture2D texture)
+        {
+            this.spriteTexture = texture;
+            this.spritePosition = location.ToVector2();
+            spriteRectangle = new Rectangle(location, size);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            spriteRectangle.Location = spritePosition.ToPoint();
+        }
+
+        public virtual void Draw(GameTime gametime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(spriteTexture, spriteRectangle, Color.White);
+        }
+    }
+}
---
+++ Game1/Game1/Sprite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    public class Sprite
+    {
+        public Vector2 Position
+        {
+            get { return spritePosition; }
+            set { spritePosition = value; }
+        }
+
+        private Rectangle spriteRectangle;
+        private Vector2 spritePosition;
+        private Texture2D spriteTexture;
+
+        public Sprite(Point size, Point position, Texture2D texture)
+        {
+            this.spriteTexture = texture;
+            this.spritePosition = position.ToVector2();
+            spriteRectangle = new Rectangle(position, size);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(GameTime gametime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(spriteTexture, spriteRectangle, Color.White);
+        }
+    }
+}
